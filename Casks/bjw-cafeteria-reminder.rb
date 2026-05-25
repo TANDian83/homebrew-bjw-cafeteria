9f7f99983dc2770c39d89a987051075b2a052093
diantan <diantan@microsoft.com>
2026-05-25 14:04:54 +0800

    add bjw-cafeteria-reminder cask formula

---
+++ Casks/bjw-cafeteria-reminder.rb
@@ -0,0 +1,15 @@
+cask "bjw-cafeteria-reminder" do
+  version "0.1.0"
+  sha256 :no_check
+
+  url "https://github.com/TANDian83/bjw-cafeteria-reminder-releases/releases/download/v#{version}/BjwCafeteriaReminder-#{version}.zip"
+  name "BJW Cafeteria Reminder"
+  desc "Menu-bar reminders for BJW Beijing West Campus cafeteria events"
+  homepage "https://github.com/TANDian83/bjw-cafeteria-reminder-releases"
+
+  app "BjwCafeteriaReminder.app"
+
+  zap trash: [
+    "~/.bjw-cafeteria",
+  ]
+end
