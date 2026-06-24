df8f3635c9a1e7ff5b4d315c3924426d3ecf5887
diantan <diantan@microsoft.com>
2026-06-24 15:09:57 +0800

    bump to 0.1.3

--- Casks/bjw-cafeteria-reminder.rb
+++ Casks/bjw-cafeteria-reminder.rb
@@ -1,5 +1,5 @@
 cask "bjw-cafeteria-reminder" do
-  version "0.1.2"
+  version "0.1.3"
   sha256 :no_check
 
   url "https://github.com/TANDian83/bjw-cafeteria-reminder-releases/releases/download/v#{version}/BjwCafeteriaReminder-#{version}.zip"
